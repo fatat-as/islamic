cc4e01dc3bd92f352de0f5fa768300d73d2f5b8e
fatat-as <angielobani@gmail.com>
2026-09-04 09:04:39 +0300

    force dynamic rendering for bee page

--- components/PDFReader.js
+++ components/PDFReader.js
@@ -142,7 +142,12 @@ const finalPdfUrl = getGoogleDrivePdfUrl(pdfUrl);
           </Document>
         </div>
         <div className="flex items-center justify-center gap-4 mt-3">
-          <button onClick={() => goToPage(1)} className="btn-primary">التالي ⬅</button>
+          <button onClick={() => goToPage(1)} className="btn-primary">التالي 
+           
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-circle" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/>
+</svg>
+          </button>
           <span className="flex items-center gap-1">
             صفحة
             <input
@@ -157,7 +162,12 @@ const finalPdfUrl = getGoogleDrivePdfUrl(pdfUrl);
             />
             من {numPages || "..."}
           </span>
-          <button onClick={() => goToPage(-1)} className="btn-primary">➡ السابق</button>
+          <button onClick={() => goToPage(-1)} className="btn-primary">
+          السابق
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+</svg>
+            </button>
         </div>
 
         <div className="text-center mt-3">
