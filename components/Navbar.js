fe33cd5e27a58fc116b5407d16ca66993fd2733f
fatat-as <angielobani@gmail.com>
2026-09-04 07:43:19 +0300

    force dynamic rendering for bee page

--- components/Navbar.js
+++ components/Navbar.js
@@ -58,7 +58,7 @@ export default function Navbar() {
 
     {/* العناصر بالعرض */}
 
-    <div className="flex flex-row flex-nowrap items-center justify-center gap-4 w-full"></div> 
+    {/* <div className="flex flex-row flex-nowrap items-center justify-center gap-4 w-full"></div>  */}
       {/* <div className={`${menuOpen ? "flex" : "hidden"} sm:flex   sm: items-start sm: items-center gap-4 px-4 pb-4 sm:py-3 border-t border-white/20 sm:border-none w-full}`}> */}
 
 {/* <div className={`flex sm:flex  sm: items-center gap-4 ${menuOpen ? "flex" : "hidden"} sm:block`}> */}
