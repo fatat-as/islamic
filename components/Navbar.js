e51cb81a10f631147791a017a9ab1a09d91f1f28
fatat-as <angielobani@gmail.com>
2026-09-04 06:21:37 +0300

    force dynamic rendering for bee page

--- components/Navbar.js
+++ components/Navbar.js
@@ -31,7 +31,7 @@ export default function Navbar() {
 
 
   return (
-    <nav className="bg-primary text-white sticky top-0 z-50 shadow-md">
+    <nav className="bg-primary text-white sticky top-0 z-50 shadow-md flex-wrap">
       <div className="max-w-6xl mx-auto px-4 py-3 flex items-center justify-between">
         <div className="flex items-center gap-2">
        <Image src="/images/badges/rih.jpg" width={60} height={60} className="font-bold rounded-full  object-cover"/>
